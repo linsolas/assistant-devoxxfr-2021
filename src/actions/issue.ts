2330a3763fe6ced5229d6bc04282e3fabe2236aa
linsolas <linsolas@gmail.com>
2021-09-22 15:22:39 +0200

    new vesion

--- src/actions/issue.ts
+++ src/actions/issue.ts
@@ -7,20 +7,17 @@ export async function onIssueCreated(context: Context) {
   if (AUTHORIZED_USERS.indexOf(user) === -1) {
     return addComment(context, `Bonjour ${user}. On se connaît ?`);
   }
-  const { title } = context.payload.issue;
-  if (/presentation/i.test(title)) {
-    addComment(
-      context,
-      `Salut @${user}. Si tu le souhaites, je peux m'en occuper.
+  addComment(
+    context,
+    `Salut @${user}. Si tu le souhaites, je peux m'en occuper.
 
 Attends une petite seconde...`,
-      5000);
-    addComment(context, `Et voici pour Alicia:
+    5000);
+
+  addComment(context, `Et voici pour Alicia:
 
 ![Alicia Stotz](${IMAGE_BASE}/alicia.png)
-`, 10000);
-    return;
-  }
+  `, 10000);
   return;
 }
 
@@ -29,9 +26,8 @@ export async function onIssueClosed(context: Context) {
   if (AUTHORIZED_USERS.indexOf(user) === -1) {
     return addComment(context, `Bonjour ${user}. On se connaît ?`);
   }
-  return addComment(context, `
-    Allez, je vous laisse, je vais aller prendre un Cappuchino-Huile de vidange au Starbucks :coffee:
+  return addComment(context, `Allez, je vous laisse, je vais aller prendre un Cappuchino-Huile de vidange au Starbucks :coffee:
 
-    A plus dans l'bug!
+A plus dans l'bug!
   `);
 }
